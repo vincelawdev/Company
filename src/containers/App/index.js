88b4a8a18ba28a49d83176683f0603c8e2da9abb
Vince Law <vince@siriusdesignlab.com.au>
2019-11-04 23:05:37 +1100

    <feat>(Components): Container and EmployeesBar components
    Create new components to display data.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,27 +1,26 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getCompanyData, getEmployeesData } from './actions';
 import Header from '../../components/Header';
+import Container from '../../components/Container';
+import EmployeesBar from '../../components/EmployeesBar';
 
 const App = () => {
-  const {
-    loading, error, // companyInfo, employees,
-  } = useSelector((state) => ({
-    loading: state.global.loading,
-    error: state.global.error,
-  }));
   const dispatch = useDispatch();
 
+  // get company and employees data
   useEffect(() => {
     dispatch(getCompanyData());
     dispatch(getEmployeesData());
   }, [dispatch]);
 
   return (
-    <Header>
-      Loading: {loading}<br />
-      Error: {error}<br />
-    </Header>
+    <>
+      <Header />
+      <Container>
+        <EmployeesBar />
+      </Container>
+    </>
   );
 };
 
