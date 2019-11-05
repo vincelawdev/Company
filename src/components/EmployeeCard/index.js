c037c386fcafbdb8aea8a20648dd0bbdf9b2614d
Vince Law <vince@siriusdesignlab.com.au>
2019-11-06 00:36:27 +1100

    <feat>(Employees): EmployeeCard
    Display employee avatar image with fallback image.

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -4,20 +4,33 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const EmployeeRow = styled.div`
+const EmployeeContainer = styled.div`
   margin: 0 15px 20px 15px;
   flex: 1 0 calc(33.333333% - 30px);
   box-sizing: border-box;
   border: 1px solid black;
 `;
 
+const EmployeeAvatar = styled.img`
+    width: 128px;
+    height: 128px;
+    margin: 0 15px 0 0;
+    float: left;
+`;
+
 const EmployeeCard = (props) => {
-  const { firstName, lastName } = props.employee;
+  const { firstName, lastName, avatar } = props.employee;
+
+  const fallbackAvatar = (ev) => {
+    // eslint-disable-next-line no-param-reassign
+    ev.target.src = 'https://via.placeholder.com/128';
+  };
 
   return (
-    <EmployeeRow>
+    <EmployeeContainer>
+      <EmployeeAvatar src={avatar} onError={fallbackAvatar} alt="{firstName} {lastName}" />
       {firstName} {lastName}
-    </EmployeeRow>
+    </EmployeeContainer>
   );
 };
 
